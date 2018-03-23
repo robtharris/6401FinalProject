aa86405197267dc0489ba5ed4d191a95e539b2cc
Rob <robert.t.harris@hotmail.com>
2018-03-23 10:41:43 -0400

    periodic push added large-hero

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested'),
+	cssImport = require('postcss-import');
+
+gulp.task('html', function() {
+	console.log('HTML Changed');
+});
+
+gulp.task('styles', function() {
+	// console.log('CSS Changed');
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+});
+
+
+gulp.task('watch', function() {
+
+	watch('./app/index.html', function() {
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('styles');
+	});
+});
