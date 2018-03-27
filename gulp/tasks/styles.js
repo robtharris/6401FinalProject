ac22ce12cc1ed34474daaf769a9868c4c57c311a
Rob <robert.t.harris@hotmail.com>
2018-03-27 14:48:14 -0400

    pushng to origin master

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested'),
+	cssImport = require('postcss-import'),
+	mixins = require('postcss-mixins');
+
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+		.on('error', function(errorInfo) {
+			console.log(errorInfo.toString()); //logs error info to console
+			this.emit('end');  //handles errors so browser-sync continues to run
+		})
+		.pipe(gulp.dest('./app/temp/styles'));
+});  //end styles task
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,7 @@
-var gulp = require('gulp'),
-	watch = require('gulp-watch'),
-	postcss = require('gulp-postcss'),
-	autoprefixer = require('autoprefixer'),
-	cssvars = require('postcss-simple-vars'),
-	nested = require('postcss-nested'),
-	cssImport = require('postcss-import');
-
-gulp.task('html', function() {
-	console.log('HTML Changed');
-});
-
-gulp.task('styles', function() {
-	// console.log('CSS Changed');
-	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-		.pipe(gulp.dest('./app/temp/styles'));
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
 
-gulp.task('watch', function() {
 
-	watch('./app/index.html', function() {
-		gulp.start('html');
-	});
 
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('styles');
-	});
-});
+
